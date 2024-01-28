ae9d18707171cd1be05450ed0534ed826554c493
Raghadkatout <ragahdkatout67@gmail.com>
2024-01-28 23:12:40 +0300

    newLS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 let allEmp = [];
 let empByDep = {};
@@ -15,22 +15,24 @@ function Employee(id, name, department, level, imageurl) {
     this.salry = 0;
     allEmp.push(this);
 
-
     if (!empByDep[department]) {
         empByDep[department] = [];
     }
 
     empByDep[department].push(this);
+}
+
+function staticEmp() {
+    
+        new Employee(1000, "Ghazi Samer", "Administration", "Senior", "./assets/img1.jpg");
+        new Employee(1001, "Lana Ali", "Finance", "Senior", "./assets/img1.jpg");
+        new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "./assets/img1.jpg");
+        new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", "./assets/img1.jpg");
+        new Employee(1004, "Omar Zaid", "Development", "Senior", "./assets/img1.jpg");
+        new Employee(1005, "Rana Saleh", "Development", "Junior", "./assets/img1.jpg");
+        new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "./assets/img1.jpg");
 
 }
-new Employee(1000, "Ghazi Samer", "Administration", "Senior", "./assets/img1.jpg")
-new Employee(1001, "Lana Ali", "Finance", "Senior", "./assets/img1.jpg")
-new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "./assets/img1.jpg")
-new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", "./assets/img1.jpg")
-new Employee(1004, "Omar Zaid", "Development", "Senior", "./assets/img1.jpg")
-new Employee(1005, "Rana Saleh", "Development", "Junior", "./assets/img1.jpg")
-new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "./assets/img1.jpg")
-
 
 console.log(allEmp);
 
@@ -53,34 +55,22 @@ Employee.prototype.calculateSalary = function () {
 
 Employee.prototype.generateUniuqeId = function () {
     return Math.floor(1000 + Math.random() * 9000);
-}
+};
 
 Employee.prototype.render = function () {
-
     let cardElement = document.createElement("div");
     cardElement.classList.add("emp-card");
 
     let contentElement = `
-
-    <img class="emp-img" src="${this.imageURL}" alt="${this.fullName}">
-    <p>Name: ${this.fullName} - ID: ${this.empId}</p> 
-    <p>Department: ${this.departmentEmp} - Level: ${this.level}</p>
-    <p>${this.calculateSalary()}</p>
+        <img class="emp-img" src="${this.imageURL}" alt="${this.fullName}">
+        <p>Name: ${this.fullName} - ID: ${this.empId}</p> 
+        <p>Department: ${this.departmentEmp} - Level: ${this.level}</p>
+        <p>${this.calculateSalary()}</p>
     `;
 
     cardElement.innerHTML = contentElement;
-    divElement.appendChild(cardElement)
+    divElement.appendChild(cardElement);
 };
-
-function findDepartmentSection(department) {
-    let departmentSections = document.querySelectorAll(".department-section h2");
-    for (let section of departmentSections) {
-        if (section.textContent === department) {
-            return section.parentNode;
-        }
-    }
-    return null;
-}
 
 function renderEmpByDep(department) {
     let divDepartment = document.createElement("div");
@@ -91,48 +81,107 @@ function renderEmpByDep(department) {
     divDepartment.appendChild(h2Department);
     divElement.appendChild(divDepartment);
 
-    if (empByDep[department]) {
-        empByDep[department].sort((a, b) => a.empId - b.allEmp);
+    empByDep[department].forEach(employee => {
+        let cardElement = document.createElement("div");
+        cardElement.classList.add("emp-card");
+
+        let contentElement = `
+            <img class="emp-img" src="${employee.imageURL}" alt="${employee.fullName}">
+            <p>Name: ${employee.fullName} - ID: ${employee.empId}</p> 
+            <p>Department: ${employee.departmentEmp} - Level: ${employee.level}</p>
+            <p>${employee.calculateSalary()}</p>
+        `;
+
+        cardElement.innerHTML = contentElement;
+        divDepartment.appendChild(cardElement);
+    });
+}
+
+function findDepartmentSection(department) {
+    let departmentSections = document.querySelectorAll(".department-section h2");
+    for (let section of departmentSections) {
+        if (section.textContent === department) {
+            return section.parentElement; // return the parent div
+        }
+    }
+    return null;
+}
+
+
+// Step 1: create function to save data in LS
+function saveData(data) {
+    let stringArr = JSON.stringify(data);
+    localStorage.setItem("Employees", stringArr);
+}
+
+// Step 2: create function to get data from LS
+function getData() {
+    let retrievedArr = localStorage.getItem("Employees");
+    let objArr = JSON.parse(retrievedArr);
+    console.log("Array after Parse: " + objArr);
+
         
-        empByDep[department].forEach(
-            employee => {
-                employee.render();
-            });
+
+        for (let i = 7; i < objArr.length; i++) {
+            new Employee(objArr[i].empId, objArr[i].fullName, objArr[i].departmentEmp, objArr[i].level, objArr[i].imageURL);
+    
     }
 
-
+    renderAll();
 }
+
+staticEmp();
 
 for (let department in empByDep) {
     renderEmpByDep(department);
 }
+function renderAll()
+{
+    for (let i = 0; i < allEmp.length; i++) {
+    // allEmp[i].render();
+    allEmp[i].render();
+}
+}
 
-// for (let i = 0; i < allEmp.length; i++) {
-//     allEmp[i].render();
-// }
-
-formElement.addEventListener("submit", submitHandler)
+formElement.addEventListener("submit", submitHandler);
 
 function submitHandler(event) {
     event.preventDefault();
-    // console.log(event);
+
     let idEmp = allEmp[0].generateUniuqeId();
     let name = event.target.fName.value;
     let department = event.target.department.value;
     let level = event.target.levelEmp.value;
     let image1 = event.target.imageURL.value;
 
-    // let price = calculateSalary();
-
     let newObject = new Employee(idEmp, name, department, level, image1);
 
     console.log(newObject);
-    newObject.render();
+    // newObject.render();
+    saveData(allEmp);
 
     let departmentSection = findDepartmentSection(department);
-    if (departmentSection) {
-        // newObject.render();
-        departmentSection.appendChild(divElement.lastChild);
+    if (!departmentSection) {
+        departmentSection = document.createElement("div");
+        departmentSection.classList.add("department-section");
+        let h2Department = document.createElement("h2");
+        h2Department.textContent = department;
+        departmentSection.appendChild(h2Department);
+        divElement.appendChild(departmentSection);
     }
 
+    let cardElement = document.createElement("div");
+    cardElement.classList.add("emp-card");
+
+    let contentElement = `
+        <img class="emp-img" src="${newObject.imageURL}" alt="${newObject.fullName}">
+        <p>Name: ${newObject.fullName} - ID: ${newObject.empId}</p> 
+        <p>Department: ${newObject.departmentEmp} - Level: ${newObject.level}</p>
+        <p>${newObject.calculateSalary()}</p>
+    `;
+
+    cardElement.innerHTML = contentElement;
+    departmentSection.appendChild(cardElement);
 }
+
+getData();
